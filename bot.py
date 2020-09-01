7709b9dc1d613405a3e185653b773358dcc0c076
alirezaninja <65853938+alirezaninja@users.noreply.github.com>
2020-09-02 02:54:10 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,17 +5,35 @@ bot = telebot.TeleBot(TOKEN)
 #--------------#
 
 @bot.message_handler(content_types=['text'])
+
 def botmain(user):
 	usertext = user.text
 	userchatid = user.chat.id
-	username = user.chat.id
+	username = user.chat.username
 	userfirstname = user.chat.first_name
 	userlastname = user.chat.last_name
-	if (userchatid == 1017719550):
-		bot.send_message(userchatid,"Salam " + userfirstname)
+
+	#--------------#
+
+	print("\nUserChatID => " + str(userchatid))
+
+	if(username == None):
+		pass
 	else:
-		bot.send_message(userchatid , userfirstname + " Shoma Ejaze Estefade az Robat \"Alireza'sOSremoter\" ra Nadarid.")
-	
+		print("UserName => @" + str(username))
+
+	print("FirstName => " + str(userfirstname))
+	print("UserTxt => " + str(usertext))
+	print("\n#--------------# \n\n")
+
+
+	#--------------#
+
+	if (userchatid == 1017719550):
+		if(usertext == "/start"):
+			bot.send_message(userchatid , "Slm Admin \"" + str(userfirstname) +"\"")
+	else:
+		bot.send_message(userchatid , "\"" +userfirstname +"\"" + " Aziz: \n Shoma Ejaze Estefade az Robat \"Alireza'sOSremoter\" ra Nadarid.\n\nBaray Estefade be ID @UrmiaCoder Moraje Farmaeid.")
 
 #---------------#
 bot.polling(True)
