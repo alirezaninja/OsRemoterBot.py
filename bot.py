cf61574da6ac543528c37f4bbad5effe5c92f727
alirezaninja <65853938+alirezaninja@users.noreply.github.com>
2020-09-02 19:23:48 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,8 @@
 import telebot
+from telebot import types
+import os
+print(" * Bot is Online *\n")
+print("#--------------#")
 #---------------#
 TOKEN = "1250722307:AAFEGLBcdLHd6h1IHi37ttS0Tzddffq2SEk"
 bot = telebot.TeleBot(TOKEN)
@@ -15,7 +19,16 @@ def botmain(user):
 
 	#--------------#
 
-	print("\nUserChatID => " + str(userchatid))
+	Dokmeha = types.ReplyKeyboardMarkup(resize_keyboard=True)
+	Dokme1 = types.KeyboardButton("ScreenShot")
+	Dokme2 = types.KeyboardButton("PowerOptions")
+	Dokme3 = types.KeyboardButton("PlaySound")
+	Dokme4 = types.KeyboardButton("FillManager")
+	Dokmeha.add(Dokme1 , Dokme2 , Dokme3 , Dokme4)
+
+	#--------------#
+
+	print("Command Send From:\nUserChatID => " + str(userchatid))
 
 	if(username == None):
 		pass
@@ -26,14 +39,13 @@ def botmain(user):
 	print("UserTxt => " + str(usertext))
 	print("\n#--------------# \n\n")
 
-
 	#--------------#
 
 	if (userchatid == 1017719550):
 		if(usertext == "/start"):
-			bot.send_message(userchatid , "Slm Admin \"" + str(userfirstname) +"\"")
+			bot.send_message(userchatid , "Slm Admin \"" + str(userfirstname) +"\"\n\n-Chat Off-\n\nEstefade az Robat Fagat ba Dokmeha." , reply_markup=Dokmeha)		
 	else:
-		bot.send_message(userchatid , "\"" +userfirstname +"\"" + " Aziz: \n Shoma Ejaze Estefade az Robat \"Alireza'sOSremoter\" ra Nadarid.\n\nBaray Estefade be ID @UrmiaCoder Moraje Farmaeid.")
+		bot.send_message(userchatid , "\"" +userfirstname +"\"" + " Aziz: \n Shoma Ejaze Estefade az Robat \"Alireza'sOSremoter\" ra Nadarid.\n\nBaray Estefade be ID @UrmiaCoder Moraje Farmaeid.\n\n-Chat Off-" , reply_markup=Dokmeha)		
 
 #---------------#
 bot.polling(True)
